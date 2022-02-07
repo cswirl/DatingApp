cc5e9815c9275d5d361c7a7e0d3017c9a85e6148
cswirl <jay.angeles@protonmail.com>
2022-02-07 16:27:24 -0500

    Persist the changes in the API
    - Member Details. use a DTO and automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -33,12 +34,31 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        // api/users/3 - where 3 is the id value
+        // This method will have a url of ./api/user/username
+        // username is a parameter
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
 
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            // This will give us the user's username from the token that the API uses to authenticate this user
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            // This is automatic version of assigning user object the same properties existing in the memberUpdateDto object
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
