15b3aa1a153330a18d5217d73c4fefed1d1397fa
cswirl <jay.angeles@protonmail.com>
2022-02-07 15:42:34 -0500

    Add a CanDeactivate route guard - 2
    - works within Angular level - not browser level

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
   canDeactivate(
-    component: unknown,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    component: MemberEditComponent): boolean {
+      if (component.editForm.dirty) {
+        return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
+      }
     return true;
   }
   
