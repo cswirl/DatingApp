790574de7003e869b2448e9238cc8521b601f231
cswirl <jay.angeles@protonmail.com>
2022-02-17 22:53:34 -0500

    Restore the caching for Member Details via reduce

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -39,8 +39,14 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    const member = this.members.find(member => member.username === username);
-    if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()]
+    .reduce((arr, elem) => arr.concat(elem.result), [])
+    .find((member: Member) => member.username === username);
+
+    if(member) {
+      return of(member);
+    }
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
