9997918b49f183a255d970e71602e6efbadd52af
cswirl <jay.angeles@protonmail.com>
2022-02-22 22:32:39 -0500

    Get the Messages - Inbox (received) and Outbox (sent)

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -54,6 +55,18 @@ namespace API.Controllers
 
             return BadRequest("Failed to send message");
 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages);
+
+            return messages;
         }
     }
 }
