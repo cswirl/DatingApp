1ff998e182ca854ae5593cea9c30fa16c6a28ab5
cswirl <jay.angeles@protonmail.com>
2022-03-09 23:05:11 -0500

    Optimize the message thread

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -43,6 +43,14 @@ namespace API.Data
             return await _context.Connections.FindAsync(connectionId);
         }
 
+        public async Task<Group> GetGroupForConnection(string connectionId)
+        {
+            return await _context.Groups
+                   .Include(g => g.Connections)
+                   .Where(g => g.Connections.Any(x => x.ConnectionId == connectionId))
+                   .FirstOrDefaultAsync();
+        }
+
         public async Task<Message> GetMessage(int id)
         {
             return await _context.Messages
