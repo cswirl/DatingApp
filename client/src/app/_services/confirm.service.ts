e0752418b9b89af00ae732098da81ba8e59e0996
cswirl <jay.angeles@protonmail.com>
2022-03-12 19:56:54 -0500

    Add Confirm service and Modal in the Angular app - 2
    - Unsaved Changes in Edit Profile
    - Message deletion in the Messages

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,5 +1,8 @@
+import { LiteralExpr } from '@angular/compiler';
 import { Injectable, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +18,7 @@ export class ConfirmService {
   // }
  
   confirm(title = 'Confirmation', message = 'Are you sure you want to do this?', 
-  btnOkText = 'Ok', btnCancelText = 'Cancel'): void {
+  btnOkText = 'Ok', btnCancelText = 'Cancel'): Observable<boolean> {
     const config = {
       initialState: {
         title,
@@ -25,10 +28,29 @@ export class ConfirmService {
       }
     }
     
-    this.bsModelRef = this.modalService.show('confirm', config)
-    
+    this.bsModelRef = this.modalService.show(ConfirmDialogComponent, config);
+    this.bsModelRef.onHide
+
+    return new Observable<boolean>(this.getResult());
+
     // this.message = 'Confirmed!';
     // this.modalRef?.hide();
+  }
+
+  private getResult() {
+    return (observer) => {
+
+      const subscription = this.bsModelRef.onHidden.subscribe(() => {
+        observer.next(this.bsModelRef.content.result);
+        observer.complete();
+      })
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      }
+    }
   }
  
   decline(): void {
