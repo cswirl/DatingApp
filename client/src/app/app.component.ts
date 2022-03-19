f2f02ee199752da7a1554ba650e8a046bb491640
cswirl <jay.angeles@protonmail.com>
2022-03-19 18:42:07 -0400

    Member Detail move Interest-tab inside About. Change Title
    - Rearrange nav links

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { PresenceService } from './_services/presence.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating app';
+  title = 'Collide';
   users: any;
 
   constructor(private accountService: AccountService, private presence: PresenceService) {
